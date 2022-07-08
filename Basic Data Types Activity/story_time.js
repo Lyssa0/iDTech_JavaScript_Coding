bbd85d977745c38b06868a69de6fab5a3178ed31
Lyssa0e <aizzalizza@gmail.com>
2022-07-08 13:31:59 -0500

    Added Assignment

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,38 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+var protagonistName = "Shrek";
+var storySetting = "Donkey's Sweet Shop";
+var protagonistObjective = "to find the gummy sharks";
+var storyTitle = "Shrekcellent Search";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+var protagonistAge = 106;
+var year = 1600;
+var century = "16th century";
+
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+var possesions = 
+["26 silver coins", " 1 fancy fedora", " 4 banjos"];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+var hungryForGummySharks = true;
+if (hungryForGummySharks === true)
+{
+ var hungryForGummySharks = "Shrek is very hungry"
+}
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log (protagonistName + " loves nothing more than to snack on gummy sharks as he plays his banjo.");
+console.log ("They have been his favorite candy for the past " + protagonistAge + " years of his life.");
+console.log("But, oh no! He ran out.")
+console.log("Because it is the " + century + ", " + protagonistName + " can not order more scrumptious gummy sharks from amazon, so he walks to " + storySetting + ".");
+console.log("As he arrives, he enters the doorway with " + possesions + ", and a goal in his heart: " + protagonistObjective + ".");
+console.log("He begins his "+ storyTitle + " in isle "+ year + ".");
+console.log(hungryForGummySharks + ", so as soon as he lays his eyes on the gummy section, he snatches every single container on the shelf, pays, and bolts back home.")
+console.log("Thus, he was finally able to spend the rest of his day happily snacking on gummy sharks and playing his banjo...")
+console.log("The End.")
